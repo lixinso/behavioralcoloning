52a9b13bca0303d96eab82a5cbae77cba1e17a3e
Xinsong Li <lixinso@gmail.com>
2017-01-24 23:09:25 -0600

    resize to 200,66 same as nvidia paper. With changing y to 5. The val acc = 0.7395

--- image_resize_test.py
+++ image_resize_test.py
@@ -6,8 +6,8 @@ def resize_file(file_name):
 
     im = Image.open("../data/IMG/" + file_name)
     #im.thumbnail((80,160),Image.ANTIALIAS)
-    img1 = im.resize((80, 40))
-    img1.save("../data/IMG_resized/" + file_name)
+    img1 = im.resize((200, 66))
+    img1.save("../data/IMG_resized_nvidia/" + file_name)
 
 files = os.listdir("../data/IMG/")
 for file in files:
